169b653c7f990b2ed9e3bc047d2000ef923d1308
Abdullah Maqbool - Coding Enthusiast <maqboolmianabdullah@gmail.com>
2025-05-10 22:21:09 +0500

    Add files via upload

---
+++ PalindromeLinkedList.java
@@ -0,0 +1,56 @@
+// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+// Example 1:
+// Input: head = [1,2,2,1]
+// Output: true
+
+// Example 2:
+// Input: head = [1,2]
+// Output: false
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+         return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverseList(slow);
+
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+
+    ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    } 
+}
